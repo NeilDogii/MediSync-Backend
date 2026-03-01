c871edd383ae3280bf5a79454557beec12c4a8d6
NeilDogii <aryaworrior41@gmail.com>
2026-03-02 02:03:07 +0530

    Modify | admin doctor management api

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('admin')
 export class AdminController {
@@ -8,5 +17,23 @@ export class AdminController {
   @Get('/doctors')
   async getDoctors() {
     return await this.adminService.fetchDoctors();
+  }
+
+  @Post('/doctors')
+  async createDoctor(@Body() data: Prisma.DoctorCreateInput) {
+    return await this.adminService.createDoctor({ data });
+  }
+
+  @Patch('/doctors/:id')
+  async updateDoctor(
+    @Body() data: Prisma.DoctorUpdateInput,
+    @Param('id') id: string,
+  ) {
+    return await this.adminService.updateDoctor({ id, data });
+  }
+
+  @Delete('/doctors/:id')
+  async deleteDoctor(@Param('id') id: string) {
+    return await this.adminService.deleteDoctor(id);
   }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,8 +1,11 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { database } from '../common/utils/database.util';
+import { Prisma } from '@prisma/client';
+import { EncryptUtil } from '../common/utils/encrypt.util';
 
 @Injectable()
 export class AdminService {
+  constructor(private readonly encryptUtil: EncryptUtil) {}
   private readonly db = database;
 
   async fetchDoctors() {
@@ -16,6 +19,78 @@ export class AdminService {
     } catch (error) {
       throw new HttpException(
         'Failed to fetch doctors: ' + error,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async createDoctor({ data }: { data: Prisma.DoctorCreateInput }) {
+    if (!data) {
+      throw new HttpException(
+        'Doctor data is required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    try {
+      const doctor = await this.db.doctor.create({
+        data,
+      });
+
+      return doctor;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to create doctor: ' + error,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async updateDoctor({
+    id,
+    data,
+  }: {
+    id: string;
+    data: Prisma.DoctorUpdateInput;
+  }) {
+    if (!data || Object.keys(data).length === 0 || !id || isNaN(Number(id))) {
+      throw new HttpException(
+        'Doctor data is required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    try {
+      if (data.password) {
+        data.password = await this.encryptUtil.encryptPayload(
+          data.password as string,
+        );
+      }
+      const doctor = await this.db.doctor.update({
+        where: { id: Number(id) },
+        data,
+      });
+      return doctor;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to update doctor: ' + error,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async deleteDoctor(id: string) {
+    if (!id || isNaN(Number(id))) {
+      throw new HttpException(
+        'Valid doctor ID is required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    try {
+      return await this.db.doctor.delete({
+        where: { id: Number(id) },
+      });
+    } catch (error) {
+      throw new HttpException(
+        'Failed to delete doctor: ' + error,
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
